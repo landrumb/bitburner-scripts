586594c9ab3ab48f5edd306f40c931e00523fd22
landrumb <36489943+landrumb@users.noreply.github.com>
2022-03-05 14:33:26 -0500

    added timing argument

--- src/weaken.ts
+++ src/weaken.ts
@@ -1,5 +1,9 @@
 import { NS } from '@ns'
 
 export async function main(ns: NS): Promise<void> {
+  // optional second argument provides a time when the call should execute
+  if (ns.args.length > 1) {
+    await ns.sleep(ns.args[1] as number - Date.now())
+  }
   await ns.weaken(ns.args[0] as string);
 }
