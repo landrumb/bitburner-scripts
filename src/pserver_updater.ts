8f8ec1420a08930019600968705a38aec0482b12
landrumb <36489943+landrumb@users.noreply.github.com>
2022-03-05 14:34:43 -0500

    actually works now

--- src/pserver_updater.ts
+++ src/pserver_updater.ts
@@ -7,37 +7,56 @@ function getMaxThreads(ns: NS, host: string, program: string): number {
 export async function main(ns: NS): Promise<void> {
   // purchase new pservers and run hack_horse.js with them.
   const pservs = new Array<string>(25);
-  let z = Math.max(2, Math.floor(Math.log2(ns.getPlayer().money / 55000)) - 1);
+  let z = Math.min(Math.max(2, Math.floor(Math.log2(ns.getPlayer().money / 55000)) - 1), 20);
 
-  const moneyThreshold = ns.getServerMaxMoney(ns.args[0] as string) * 0.75;
-  const securityThreshold = ns.getServerBaseSecurityLevel(ns.args[0] as string) + 5;
+  const moneyThreshold = ns.getServerMaxMoney(ns.args[0] as string);
+  const securityThreshold = ns.getServerMinSecurityLevel(ns.args[0] as string);
+
+  ns.disableLog("sleep");
 
   // get all existing servers, and set z to the highest power present in the existing servers.
   const existing = ns.getPurchasedServers();
-  const indexRegex = /^pserv-(\d+)/mg;
+  const indexRegex = /^pserv-(\d+)/m;
   for (const server of existing) {
     const match = indexRegex.exec(server);
-    if (!(match === null) && (match[1] as unknown as number) <= 24) {
+
+    if (!(match === null) && (parseInt(match[1]) as number) <= 24) {
       pservs[match[1] as unknown as number] = server;
       z = Math.max(z, Math.floor(Math.log2(ns.getServerMaxRam(server))));
     }
   }
+  // ns.tprint(ns.getPurchasedServers());
+  // ns.tprint(pservs);
 
   while (z <= 20) {
-    const cost = (2 ** z) * 55000;
+    const cost = (2 ** z) * (55000 + 5000);
     for (let i = 0; i < 25; i++) {
       // wait for adequate funds
       while (ns.getPlayer().money < cost) {
-        await ns.sleep(1000); // wait 1 second
+        ns.print(`Waiting for ${cost.toLocaleString()} money`);
+        await ns.sleep(1000 * 60); // wait 60 seconds
+      }
+
+      // if ((pservs[i] !== undefined && pservs[i].length < 2)) {
+      //   continue;
+      // }
+
+      if (pservs[i] !== undefined && ns.getServerMaxRam(pservs[i]) >= 2 ** z) {
+        continue;
       }
 
       // purchase server
-      ns.deleteServer(pservs[i])
-      pservs[i] = ns.purchaseServer(`pserv-${i}-${2 ** z}GB`, 2 ** z);
+      if (!(pservs[i] === undefined || pservs[i] == "home")) {
+        ns.killall(pservs[i])
+        ns.deleteServer(pservs[i])
+      }
+      pservs[i] = ns.purchaseServer(`pserv-${i}-${(2 ** z).toLocaleString()}GB`, 2 ** z);
 
-      // run hack_horse.js on server
-      await ns.scp("hack_horse.js", "home", pservs[i]);
-      ns.exec("hack_horse.js", pservs[i], getMaxThreads(ns, pservs[i], "hack_horse.js"), ns.args[0], moneyThreshold, securityThreshold);
+      // run batching.js on server
+      await ns.scp(["batching.js", "grow.js", "hack.js", "weaken.js"], "home", pservs[i]);
+      ns.exec("batching.js", pservs[i], 1, ns.args[0]);
+
+
     }
     z++;
   }
